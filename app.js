68b9789801b7bf1803bdcdd08dfbed26f9d81e61
annika <annikaslund@gmail.com>
2019-03-06 12:14:17 -0800

    Add route handler /mean

--- app.js
+++ app.js
@@ -6,6 +6,45 @@ const app = express();
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/mean', function(req, res, next){
+    try{
+        let nums = req.query.nums;
+        let numsArr = nums.split(",").map(function(num){ return parseInt(num) });
+        
+        if(numsArr.includes(NaN)){
+            throw new ExpressError("Input must be integers", 400);
+        }
+    
+        let numsTotal = numsArr.length;
+        let total = numsArr.reduce(function(acc, num){ return acc + num; })
+
+        let mean = total / numsTotal;
+
+        return res.json({mean});
+
+    } catch(err) {
+        return next(err);
+    }
+})
+
+
+// app.get('/median', function(req, res, next){
+//     try{
+
+//     } catch(err) {
+        
+//     }
+// })
+
+
+// app.get('/mode', function(req, res, next){
+//     try{
+
+//     } catch(err) {
+        
+//     }
+// })
+
 app.use(function (req, res, next) {
     const notFoundError = new ExpressError("Not Found", 404);
     return next(notFoundError)
